2c9cc8e62750aeda141191c1de79bbd5ede00333
Sergey <s.tochenyuk@gmail.com>
2024-03-04 16:04:02 +0400

    Создаёт остальное

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
 /* в этот файл добавляет скрипты*/
+
+/* Menu */
+const menuBtn = document.querySelector('.main-nav__button');
+const menuNav = document.querySelector('.main-nav');
+
+
+if (menuBtn) {
+  menuBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    menuNav.classList.toggle('main-nav--open');
+    menuNav.classList.toggle('main-nav--close');
+  });
+}
+
+/* Slider */
